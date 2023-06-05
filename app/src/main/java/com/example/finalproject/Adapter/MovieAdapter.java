589f82532255a1d4042dc4bc8589399b45b11954
SyifaUrRahmi <sifasisi3@gmail.com>
2023-06-05 22:19:48 +0800

    Membuat MovieAdapter

---
+++ app/src/main/java/com/example/finalproject/Adapter/MovieAdapter.java
@@ -0,0 +1,62 @@
+package com.example.finalproject.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.finalproject.Data.MovieResponse;
+import com.example.finalproject.R;
+import com.example.finalproject.fragment.MoviesFragment;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    List<MovieResponse> movieResponses;
+
+    public MovieAdapter(List<MovieResponse> movieResponses){
+        this.movieResponses = movieResponses;
+    }
+
+    @NonNull
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapter.ViewHolder holder, int position) {
+        MovieResponse movieResponse = movieResponses.get(position);
+        holder.tv_judul.setText(movieResponse.getJudul());
+        holder.tv_tahun.setText(movieResponse.getWaktu());
+        Glide.with(holder.itemView.getContext())
+                .load(movieResponse.getPoster())
+                .apply(new RequestOptions().override(350,
+                        550))
+                .into(holder.iv_gambar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieResponses.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_gambar;
+        TextView tv_judul, tv_tahun;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iv_gambar = itemView.findViewById(R.id.iv_gambar);
+            tv_judul = itemView.findViewById(R.id.tv_judul);
+            tv_tahun = itemView.findViewById(R.id.tv_tahun);
+        }
+    }
+}
