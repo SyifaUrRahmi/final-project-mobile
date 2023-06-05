1cc33c84c0b77b7470988fc63b17cbfc5c653f00
SyifaUrRahmi <sifasisi3@gmail.com>
2023-06-05 17:32:18 +0800

    Update activity main dan Atur perpindahan fragment di MainActivity.java

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
+
+import com.example.finalproject.fragment.FavoritesFragment;
+import com.example.finalproject.fragment.MoviesFragment;
+import com.example.finalproject.fragment.TVShowsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tb = findViewById(R.id.tb);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MoviesFragment moviesFragment = new MoviesFragment();
+        Fragment fragment = fragmentManager.findFragmentByTag(MoviesFragment.class.getSimpleName());
+
+        if (!(fragment instanceof MoviesFragment)) {
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container, moviesFragment,
+                            MoviesFragment.class.getSimpleName())
+                    .commit();
+        }
+        LinearLayout mv = findViewById(R.id.movies);
+        mv.setOnClickListener(movies_ -> {
+//            MoviesFragment moviesFragment = new MoviesFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_container, moviesFragment,
+                            MoviesFragment.class.getSimpleName())
+                    .addToBackStack(null)
+                    .commit();
+        });
+        LinearLayout ts = findViewById(R.id.ts);
+        ts.setOnClickListener(tvshows_ -> {
+            TVShowsFragment tvShowsFragment = new TVShowsFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_container, tvShowsFragment,
+                            TVShowsFragment.class.getSimpleName())
+                    .addToBackStack(null)
+                    .commit();
+        });
+        LinearLayout fav = findViewById(R.id.fav);
+        fav.setOnClickListener(fav_ -> {
+            FavoritesFragment favoritesFragment = new FavoritesFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frame_container, favoritesFragment,
+                            FavoritesFragment.class.getSimpleName())
+                    .addToBackStack(null)
+                    .commit();
+        });
     }
 }
