90905e6bc39304ba81b9f8ca18f1365df8c4c2cd
SyifaUrRahmi <sifasisi3@gmail.com>
2023-06-10 07:26:44 +0800

    Update tampilan

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toolbar;
@@ -17,12 +20,19 @@ import com.example.finalproject.fragment.TVShowsFragment;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tb;
+    private ImageView movie, tv_show, favorit;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tb = findViewById(R.id.tb);
+
+        movie = findViewById(R.id.movie);
+        tv_show = findViewById(R.id.tv_show);
+        favorit = findViewById(R.id.fav_);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         MoviesFragment moviesFragment = new MoviesFragment();
@@ -37,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         }
         LinearLayout mv = findViewById(R.id.movies);
         mv.setOnClickListener(movies_ -> {
+            movie.setBackground(getDrawable(R.drawable.bg_elips));
+            tv_show.setBackgroundColor(Color.parseColor("#6FCFFA"));
+            favorit.setBackgroundColor(Color.parseColor("#6FCFFA"));
             tb.setText("Movies");
             fragmentManager
                     .beginTransaction()
@@ -47,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
         });
         LinearLayout ts = findViewById(R.id.ts);
         ts.setOnClickListener(tvshows_ -> {
+            tv_show.setBackground(getDrawable(R.drawable.bg_elips));
+            favorit.setBackgroundColor(Color.parseColor("#6FCFFA"));
+            movie.setBackgroundColor(Color.parseColor("#6FCFFA"));
             tb.setText("TV Shows");
             TVShowsFragment tvShowsFragment = new TVShowsFragment();
             fragmentManager
@@ -58,6 +74,9 @@ public class MainActivity extends AppCompatActivity {
         });
         LinearLayout fav = findViewById(R.id.fav);
         fav.setOnClickListener(fav_ -> {
+            movie.setBackgroundColor(Color.parseColor("#6FCFFA"));
+            tv_show.setBackgroundColor(Color.parseColor("#6FCFFA"));
+            favorit.setBackground(getDrawable(R.drawable.bg_elips));
             tb.setText("Favorites");
             FavoritesFragment favoritesFragment = new FavoritesFragment();
             fragmentManager
